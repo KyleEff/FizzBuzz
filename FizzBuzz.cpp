bdc45c3fe7c81a69111fb7efce5b9a4d1d2c25d3
KyleEff <75494641+KyleEff@users.noreply.github.com>
2020-12-31 16:36:56 -0600

    Add files via upload

---
+++ FizzBuzz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int num{ 1 };
+
+	for (num; num <= 100; num++) {
+		//if (num % 3 == 0 && num % 5 == 0) {
+		if (num % 15 == 0) {
+			std::cout << "Fizz Buzz" << std::endl;
+		}
+		else if (num % 3 == 0) {
+			std::cout << "Fizz" << std::endl;
+		}
+		else if (num % 5 == 0) {
+			std::cout << "Buzz" << std::endl;
+		}
+		else {
+			std::cout << num << std::endl;
+		}
+	}
+	return 0;
+}
